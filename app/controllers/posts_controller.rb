eb1796373dd08dd5dfc9af2d03118c7c430f9c3e
PJ Davis <pj.davis@gmail.com>
2025-03-12 15:37:31 -0400

    Interview Work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,18 @@ class PostsController < ApplicationController
     end
 
     def create
+        @post = Post.new(post_params)
+        if @post.save
+          flash.now[:notice] = "Post successfully created!"
+        else
+          flash.now[:alert] = "Post could not be created!"
+          render status: 422
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :description, :body)
     end
 end
